09bbbcb8c81014e7d1efe5231c818ae1a54837fd
Muhammad Arham <67412607+Arham-12336@users.noreply.github.com>
2022-12-27 13:59:28 +0500

    Initial commit

---
+++ SuperStore/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace ImtiajSuperStore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.xGrid = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblBill = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.xGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search-Item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Item-Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Item-Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Quantity:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(139, 70);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(76, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "No-Data";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(139, 101);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(51, 20);
+            this.lblPrice.TabIndex = 0;
+            this.lblPrice.Text = "0 Rs.";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(139, 26);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(331, 26);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyDown);
+            // 
+            // txtQty
+            // 
+            this.txtQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQty.Location = new System.Drawing.Point(124, 134);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 26);
+            this.txtQty.TabIndex = 1;
+            this.txtQty.Text = "1";
+            this.txtQty.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtQty_KeyDown);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(392, 58);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(78, 32);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // xGrid
+            // 
+            this.xGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.xGrid.Location = new System.Drawing.Point(12, 166);
+            this.xGrid.Name = "xGrid";
+            this.xGrid.Size = new System.Drawing.Size(472, 316);
+            this.xGrid.TabIndex = 3;
+            this.xGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.xGrid_CellDoubleClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(318, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total-Bill:";
+            // 
+            // lblBill
+            // 
+            this.lblBill.AutoSize = true;
+            this.lblBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBill.ForeColor = System.Drawing.Color.Red;
+            this.lblBill.Location = new System.Drawing.Point(408, 134);
+            this.lblBill.Name = "lblBill";
+            this.lblBill.Size = new System.Drawing.Size(51, 20);
+            this.lblBill.TabIndex = 5;
+            this.lblBill.Text = "0 Rs.";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(370, 488);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 31);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save  Order";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // BarCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 521);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblBill);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.xGrid);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BarCode";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imtiaj Super Store";
+            ((System.ComponentModel.ISupportInitialize)(this.xGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView xGrid;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblBill;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SuperStore/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImtiajSuperStore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            xGrid.Columns.Add("A", "Item-Name");
+            xGrid.Columns.Add("B", "UPrice");
+            xGrid.Columns.Add("C", "Qty");
+            xGrid.Columns.Add("D", "Total");
+            xGrid.Columns[0].Width = 200;
+            xGrid.Columns[1].Width = 50;
+            xGrid.Columns[2].Width = 50;
+            xGrid.Columns[3].Width = 50;
+            GetProducts();
+        }
+        private void Calc_Bill()
+        {
+            int Sum = 0;
+            for (int i = 0; i < xGrid.Rows.Count-1; i++)
+            {
+                Sum = Sum + int.Parse(xGrid.Rows[i].Cells[3].Value.ToString());
+            }
+            lblBill.Text = Sum.ToString();
+        }
+        private void GetProducts()
+        {
+            AutoCompleteStringCollection Arr = new AutoCompleteStringCollection();
+            StreamReader SR = new StreamReader("Products.txt");
+            while (!SR.EndOfStream)
+            {
+                Arr.Add(SR.ReadLine());
+            }
+            SR.Close();
+            txtSearch.AutoCompleteCustomSource = Arr;
+            txtSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            txtSearch.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string[] A = txtSearch.Text.Split('#');
+                lblName.Text = A[0];
+                lblPrice.Text = A[1];
+                txtQty.Focus();
+            }
+        }
+        private void txtQty_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int P = int.Parse(lblPrice.Text);
+                int Q = int.Parse(txtQty.Text);
+                int T = P * Q;
+                xGrid.Rows.Add(lblName.Text, lblPrice.Text, txtQty.Text, T);
+                Calc_Bill();
+                txtSearch.Clear();
+                txtSearch.Focus();
+            }
+        }
+        private void xGrid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DialogResult DR= MessageBox.Show("Are you sure to delete item?","Delete",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (DR == DialogResult.Yes)
+            {
+                int i = xGrid.CurrentRow.Index;
+                xGrid.Rows.RemoveAt(i);
+                Calc_Bill();            
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (xGrid.Rows.Count > 1)
+            {               
+                string Invoice = DateTime.Now.ToString("ddMMMyyyyHHmmss");
+                StreamWriter SW = new StreamWriter(Invoice+".txt");
+                int Col = xGrid.Columns.Count;
+                int Row = xGrid.Rows.Count - 1;
+                for (int R = 0; R < Row; R++)
+                {
+                    for (int C = 0; C < Col; C++)
+                    {
+                        SW.Write(xGrid.Rows[R].Cells[C].Value.ToString()+"\t");
+                    }
+                    SW.WriteLine("\r\n---------------------------------------");
+                }
+                SW.WriteLine("Total Bill: " + lblBill.Text);
+                SW.Flush();
+                SW.Close();
+                xGrid.Rows.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
